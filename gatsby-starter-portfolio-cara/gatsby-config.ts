d75a48d7d39a0d02907992f3449d76cd88edbeb8
neerajkumarroy <neerajkumarroyss@gmail.com>
2024-09-18 16:06:25 +0530

    first commit

--- gatsby-starter-portfolio-cara/gatsby-config.ts
+++ gatsby-starter-portfolio-cara/gatsby-config.ts
@@ -8,9 +8,9 @@ const config: GatsbyConfig = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.mjs
-    siteTitle: `My`,
-    siteTitleAlt: ` My Portfolio`,
-    siteHeadline: `My Portfolio`,
+    siteTitle: `Cara`,
+    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
     siteUrl: `https://cara.lekoarts.de`,
     siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
     siteImage: `/banner.jpg`,
